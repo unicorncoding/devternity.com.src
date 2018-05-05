bacfc192298b3262b8fb65a679ccbdfedd8ac85f
Eduards Sizovs <eduards@sizovs.net>
2018-05-05 21:17:55 +0300

    add extra pricing

--- events/rix/2018/js/event-override.js
+++ events/rix/2018/js/event-override.js
@@ -3,15 +3,15 @@
         "pricing": {
             "packages": {
               "Main Day Only": {
-                  "price": 299,
+                  "price": 399,
                   "extra": 100,
               },
               "Power Workshop Only": {
-                  "price": 299,
+                  "price": 399,
                   "extra": 116,
               },
               "2-Days Super Pack": {
-                  "price": 599,
+                  "price": 799,
                   "extra": 200,
               }
             },
