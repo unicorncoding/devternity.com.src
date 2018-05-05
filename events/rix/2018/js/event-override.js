b381c82e51a89d06e849bc74bf5a40b3eb575ae9
Eduards Sizovs <eduards@sizovs.net>
2018-05-05 21:34:04 +0300

    fix json

--- events/rix/2018/js/event-override.js
+++ events/rix/2018/js/event-override.js
@@ -4,17 +4,17 @@
             "packages": {
               "Main Day Only": {
                   "price": 399,
-                  "extra": 100,
+                  "extra": 100
               },
               "Power Workshop Only": {
                   "price": 399,
-                  "extra": 116,
+                  "extra": 116
               },
               "2-Days Super Pack": {
                   "price": 799,
-                  "extra": 200,
+                  "extra": 200
               }
-            },
+            }
         }
     }
 ]
