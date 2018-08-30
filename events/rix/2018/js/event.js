ee7a01f88e5bceb5a9c0fca3b9a764c21213f310
Eduards Sizovs <eduards.sizovs@gmail.com>
2018-08-30 10:27:48 +0300

    step down as keynotes

--- events/rix/2018/js/event.js
+++ events/rix/2018/js/event.js
@@ -710,26 +710,10 @@
                         "title": "Coffee Break",
                         "icon": "fa-coffee"
                     },
-                        {                           
-                        "time": "18:20",                            
-                        "name": "Eduards Sizovs",
-                        "type": "speech",
-                        "twitter": "eduardsi",
-                        "country": "lv",
-                        "bio": "The Founder of DevTernity and <a href=\"https://dev.tube\">Dev.Tube</a>",
-                        "img": "images/eduardsi_vec.png",
-                        "title": "How We Built Dev.Tube",
-                        "description": "Built by 2 developers in only 2 months, Dev.Tube is gradually becoming the primary hub of developer videos. In this entertaining, but practical session, Eduards and Andrey will tell what went wrong, what went well and what can we learn from Dev.Tube launch. We’ll cover: how we attracted 1 million monthly visitors? how we maintain infrastructure (and how much does it cost $$$)? why experienced java developers ended up writing javascript? why sometimes we do not write unit tests? how to make OSS contributors happy (or angry)? how SaaS vendors helped us survive the load (and why)? and much more! After this session, you’ll be equipped with interesting real-world take-aways. Great mood guaranteed!",
-                        "tags": ["lean startup", "architecture", "case study"],
-                        "partner": {
-                          "name": "Andrey Adamovich",
-                          "img": "images/andrey_vec.png",
-                          "type": "speech",                          
-                          "country": "lv",
-                          "twitter": "codingandrey",
-                          "bio": "Co-organiser @ DevTernity, Co-founder @ <a href=\"https://dev.tube\">Dev.Tube</a>"
-                        }
-                      },
+                    {                           
+                      "time": "18:20",
+                      "type": "speech"
+                    },
                     {
                         "type": "break",
                         "time": "19:10",
